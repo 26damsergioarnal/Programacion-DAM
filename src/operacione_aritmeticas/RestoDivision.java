6fe4af6e82d85b28dbff71ccf6a66331954806fe
sergiokb <sergiokb@DESKTOP-H5HECP2>
2026-09-14 22:16:42 +0200

    Ejercicio resto division

---
+++ src/operacione_aritmeticas/RestoDivision.java
@@ -0,0 +1,16 @@
+package operacione_aritmeticas;
+
+public class RestoDivision {
+
+	public static void main(String[] args) {
+		int p = 4;
+		int m = 2;
+		int n = 6;
+		
+		System.out.println("El resultado de 4 partido de dos es: " + p / m);
+		System.out.println("y su resto es: " + p % m);
+		System.out.println("El resultado de 6 partido de 2 es: " + n / m);
+		System.out.println("y su resto es: " + n % m);
+	}
+
+}
