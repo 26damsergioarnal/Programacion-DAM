7dbaf721610c5e996bd027d984db72572e2a937d
ggkob <ggkob@DESKTOP-MFMT1G5>
2026-09-17 21:46:45 +0200

    21:45 17/09/2026

--- src/ejercicios01/Ejercicios10.java
+++ src/ejercicios01/Ejercicios10.java
@@ -4,10 +4,10 @@ public class Ejercicios10 {
 
 	public static void main(String[] args) {
 		
-		int varA = 350;
+		int varA = 100;
 		boolean multiplo5 = varA % 5 == 0;
 		boolean multiplo10 = varA % 10 == 0;
-		boolean esMayor100 = varA > 100;
+		boolean esMayor100 = varA > 100 || varA == 100;
 		boolean igual = varA == 0;
 		
 		System.out.println("El valor " + varA + "...");
@@ -19,9 +19,8 @@ public class Ejercicios10 {
 		System.out.println(multiplo10 ? "Si, es multiplo de 10" : "No, no es multiplo de 10");
 		System.out.println();
 		System.out.println("¿Es mayor que 100?");
-		System.out.print(esMayor100 ? "Si, es mayor que 100" : "No, no es mayor que 100");
-		System.out.println(igual ? ", es igual a 0" : "");
-
+		System.out.println(esMayor100 ? "Si, es mayor o igual que 100" : "No, no es mayor que 100");
+		
 	}
 
 }
---
+++ src/ejercicios01/Ejercicios11.java
@@ -0,0 +1,13 @@
+package ejercicios01;
+
+public class Ejercicios11 {
+
+	public static void main(String[] args) {
+		short cortoAlto = 32767;
+		short cortoBajo = -32767;
+		long largoAlto = 9000000000000000000L;
+		long largoBajo = -9000000000000000000L;
+
+	}
+
+}
