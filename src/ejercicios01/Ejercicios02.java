df315777d0709eead5197a285f80e84c5337da3b
ggkob <ggkob@DESKTOP-MFMT1G5>
2026-09-18 19:13:35 +0200

    18/09/2026 19:12 No se avanza AYUDAAA

--- src/ejercicios01/Ejercicios02.java
+++ src/ejercicios01/Ejercicios02.java
@@ -4,19 +4,20 @@ public class Ejercicios02 {
 
 	public static void main(String[] args) {
 		
-		//Byte - short - int - long - float - double 
+		//Byte - short - int - long - float - double - char
 		byte byteNum = 1;
 		short cortoNum = 2;
 		int intNum = 32;
 		long largoNum = 50;
 		float floatNum = 2.5f;
 		double doubleNum = 2.5;
+		char caracter = 'Ñ';
 		boolean verdadero = true;
 		// suma resta multiplicacion division resto
 		
 		System.out.println("byte + short: " + (byteNum + cortoNum));
 		System.out.println("int - long: " + (intNum - largoNum));
-		System.out.println("long * double: " + (largoNum + doubleNum));
+		System.out.println("char * double: " + (caracter * doubleNum));
 		System.out.println("int / float: " + intNum / floatNum);
 		System.out.println("Resto de int entre double: " + (intNum % doubleNum));
 		
--- src/ejercicios01/Ejercicios15.java
+++ src/ejercicios01/Ejercicios15.java
@@ -3,9 +3,14 @@ package ejercicios01;
 public class Ejercicios15 {
 
 	public static void main(String[] args) {
+		String comienza = "Comienza el programa";
+		System.out.println(comienza);
+		
 		String cadena20 = "alskrncharosteqiadomeric";
 		
-
+		
+		String acaba = "Tremina el programa";
+		System.out.println(acaba);
 	}
 
 }
